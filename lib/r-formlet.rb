98db227caf7c2db81cb4e312d1efccaaae515a82
ivanacostarubio <ivan@softwarecriollo.com>
2014-01-20 22:40:47 -0600

    boom! First commit.
    Support for basic forms. Input text, files and text areas. 
    
    Just implemented enough to create the form I need now.

---
+++ lib/r-formlet.rb
@@ -0,0 +1,52 @@
+module Formlets
+
+  def render_form
+    "<form method='post' action='#{action}'>#{render_attributes if defined? form_attributes}</form>"
+  end
+
+  private
+
+  def render_attributes
+    form_attributes.map{ |a| render_attribute a }.join
+  end
+
+  def render_attribute(a)
+    render_parent_div(a)
+  end
+
+  def render_parent_div(a)
+    "<div class='form-#{a[0]}'>#{render_input(a)}</div>"
+  end
+
+  def render_input(a)
+    send(a[1], a)
+  end
+
+  #
+  # Supported HTML Tags:
+  #
+
+  def text(a)
+    input(a)
+  end
+
+  def file(a)
+    input(a)
+  end
+
+  def textarea(a)
+    "<textarea rows='10' cols='20' id='#{a[0]}'></textarea>"
+  end
+
+  def input(a)
+    "<input type='#{a[1]}' id='#{a[0]}'></input>"
+  end
+
+  module_function
+
+  def render(klass)
+    klass.new.render_form
+  end
+end
+
+
--- spec/r-formlet_spec.rb
+++ spec/r-formlet_spec.rb
@@ -1,7 +1,114 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RFormlet" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  class BoardForm
+    include Formlets
+    def action ; "/board" ; end
+  end
+
+  class OneAttributeForm
+    include Formlets
+
+    def action
+      "/one_action"
+    end
+
+    def form_attributes
+      {
+        :name => :text
+      }
+    end
+  end
+
+  class FileForm
+    include Formlets
+
+    def action ; '/file' ; end
+
+    def form_attributes
+      {
+        :logo => :file
+      }
+    end
+  end
+
+  class TextAreaForm
+
+    include Formlets
+
+    def action; '/file' ; end
+
+    def form_attributes
+      {
+        :features => :textarea
+      }
+    end
+  end
+
+  it "points to a API endpoint" do
+    class PersonForm
+      include Formlets
+      def action ; "/person" ; end
+    end
+
+    result = "<form action='/person'></form>"
+    Formlets.render(PersonForm).should == result
+  end
+
+  it "renders another basic form" do
+    result = "<form action='/board'></form>"
+    Formlets.render(BoardForm).should == result
+  end
+
+  it "renders with one attribute text" do
+    result = "<form action='/one_action'><div class='form-name'><input type='text' id='name'></input></div></form>"
+    Formlets.render(OneAttributeForm).should == result
+  end
+
+  it "supports file uploads" do
+    result = "<form action='/file'><div class='form-logo'><input type='file' id='logo'></input></div></form>"
+    Formlets.render(FileForm).should == result
+  end
+
+  it "supports large text AKA text areas" do
+    result = "<form action='/file'><div class='form-features'><textarea rows='10' cols='20' id='features'></textarea></div></form>"
+    Formlets.render(TextAreaForm).should == result
+  end
+
+
+
+  describe "Later" do
+    it "supports PUT"
+    it "supports other HTTP verbs?"
+    it "creates the backend endpoint to create a record with this"
+    it "supports validates?"
+
+    describe "input type support" do
+      it "crashes gratefully if trying to render a type of tag that is not supported"
+      it "supports button"
+      it "supports checkbox"
+      it "supports color"
+      it "supports date "
+      it "supports datetime "
+      it "supports datetime-local "
+      it "supports email "
+      it "supports file"
+      it "supports hidden"
+      it "supports image"
+      it "supports month "
+      it "supports number "
+      it "supports password"
+      it "supports radio"
+      it "supports range "
+      it "supports reset"
+      it "supports search"
+      it "supports submit"
+      it "supports tel"
+      it "supports text"
+      it "supports time "
+      it "supports url"
+      it "supports week"
+    end
   end
 end
